17fee39deb49455cd720548d620bb215f563c09f
Przemek Pelczar <przemekpel@gmail.com>
2023-06-05 12:56:00 +0200

    update js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+{
+    const welcome = () => {
+        console.log("Witam serdecznie");
+    }
+    
+    
+    const onChangeBacgroundClick = () => {
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".themeName");
+        body.classList.toggle("dark");
+        themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
+    };
+    
+    const init = () => {
+        const button = document.querySelector(".section__button");
+        button.addEventListener("click", onChangeBacgroundClick);
+        
+        welcome();
+    };
+
+    init ()
+    
+}
